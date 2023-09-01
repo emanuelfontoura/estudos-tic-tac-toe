745bd1fe0926953adfc01589506e73da8321dfda
Emanuel Fontoura <emanuelfontoura03@gmail.com>
2023-09-01 13:51:57 -0300

    projeto finalizado

--- js/modules/play.js
+++ js/modules/play.js
@@ -2,40 +2,112 @@ export default function initPlay(){
     const playDivs = document.querySelectorAll('[data-play=div]')
     const playTurn = document.querySelector('[data-play=turn]')
     const playReset = document.querySelector('[data-play=reset]')
-    var count = 0
+    const playResult = document.querySelector('[data-play=result]')
+    let currentSelectionX = []
+    let currentSelectionO = []
+    const win = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [6, 4, 2]
+    ]
+    let canPlay = true
+    let count = 0
+    let countEqual = 0
 
-    playDivs.forEach(div => {
-        div.addEventListener('click', divHandleClick)
+    playDivs.forEach((div, index) => {
+        div.addEventListener('click', (e) => {
+            if(canPlay){
+                divHandleClick(e, index)
+            }
+        })
     })
 
     playReset.addEventListener('click', resetHandleClick)
 
-    function divHandleClick(e){
-        if(count % 2 == 0){
+    function divHandleClick(e, index){
+        if(count % 2 == 0 && !isSelected(index)){
             playTurn.innerText = 'Jogando: X'
             e.currentTarget.innerText = 'X'
-        }else{
+            currentSelectionX.push(index)
+            verifyWin(currentSelectionX, e.currentTarget.innerText)
+            count++
+            console.log(count)
+        }else if(count % 2 != 0 && !isSelected(index)){
             playTurn.innerText = 'Jogando: O'
             e.currentTarget.innerText = 'O'
+            currentSelectionO.push(index)
+            verifyWin(currentSelectionO, e.currentTarget.innerText)
+            count++
+            console.log(count)
         } 
-        nextTurn()
-        count++
+        nextTurn(index)
     }
 
-    function nextTurn(){
+    function nextTurn(index){
+        verifyDraw()
         if((count+1) % 2 == 0){
             playTurn.innerText = 'Jogando: X'
-        }else{
+        }else if ((count+1) % 2 != 0){
             playTurn.innerText = 'Jogando: O'
         } 
     }
 
     function resetHandleClick(){
-        playDivs.forEach(div => {
-            div.innerText = ''
+        if(canPlay){
+            playDivs.forEach(div => {
+                div.innerText = ''
+            })
             playTurn.innerText = 'Jogando: X'
+            currentSelectionX = []
+            currentSelectionO = []
+            count = 0
+            countEqual = 0
+        }
+    }
+
+    function verifyDraw(){
+        if(currentSelectionO.length + currentSelectionX.length === 9){
+            playResult.innerText = 'EMPATE!'
+            finishedGame()
+        }
+    }
+
+    function verifyWin(array, winner){
+        win.forEach(possibilityWin => {
+            array.forEach(a => {
+                if (possibilityWin.includes(a)){
+                    countEqual++
+                    if(countEqual == 3){
+                        canPlay = false
+                        playResult.innerText = 'Vencedor: ' + winner
+                        finishedGame()
+                    }
+                }
+            })
+            countEqual = 0
         })
-        count = 0
+    }
+
+    function finishedGame(){
+        setTimeout(() => {
+            count = 0
+            countEqual = 0
+            currentSelectionX = []
+            currentSelectionO = []
+            window.location.reload()
+        }, 1500)
+    }
+
+    function isSelected(index){
+        if(currentSelectionO.includes(index) || currentSelectionX.includes(index)){
+            return true
+        }else if(!currentSelectionO.includes(index) && !currentSelectionX.includes(index)){
+            return false
+        }
     }
 }
-
